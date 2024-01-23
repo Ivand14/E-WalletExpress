f30ec3f90d2591e56eef4271a603d554f314b071
Ivan <carivan14@homtail.com>
2024-01-23 17:41:12 -0300

    json listo

--- src/data/initialScreenData.ts
+++ src/data/initialScreenData.ts
@@ -5,15 +5,15 @@ interface information {
 export const data = [
     {
         info : "Payment Plus es una innovadora billetera virtual que facilita la compra de criptomonedas",
-        img:""
+        img:"../../assets/Image/PaymentPlus.png"
     },
     {
         info : "Podes adquirir tus criptomonedas de manera sencilla y segura",
-        img:""
+        img:"../../assets/Image/BtcLogo.png.png"
     },
     {
         info : " Ofreciéndote una experiencia cómoda y eficiente para realizar transacciones",
-        img:""
+        img:"../../assets/Image/Transacciones.png.png"
     },
 ]
 
